a7cbac2407584b2d52d38515c19922014efe6c83
Yohan Lebret <yohan.lebret@gmail.com>
2018-09-04 19:08:24 +0100

    Add unit test for store_currency_rate task

--- web/src/python_exercice/currency/tasks.py
+++ web/src/python_exercice/currency/tasks.py
@@ -7,19 +7,6 @@ from django.conf import settings
 from python_exercice.celeryapp import app
 from .models import CurrencyRate
 
-# content = {
-#   'success': True,
-#   'timestamp':1536077348,
-#   'base':'USD',
-#   'date':'2018-09-04',
-#   'rates':{
-#     'AEA':4.248562,
-#     'AFN':85.313513,
-#     'ALL':126.454847,
-#     'AMD':559.500481,
-#   }
-# }
-
 
 FIXER_CURRENCY_JSON_SCHEMA = {
     'type': 'object',
@@ -53,25 +40,29 @@ FIXER_CURRENCY_JSON_SCHEMA = {
     max_retries=3,
     name='store_currency_rate'
 )
-def store_currency_rate():
-    request = requests.get(settings.FIXER_API_ENDPOINTS)
+def store_currency_rate(api_mock_content=None):
+    content = api_mock_content
 
-    if request.status_code != 200:
-        raise Exception('Fixer Api call error, status: {}'.format(request.status_code))
-        return False
-    else:
+    if not api_mock_content:
+        request = requests.get(settings.FIXER_API_ENDPOINTS)
+
+        if request.status_code != 200:
+            raise Exception('Fixer Api call error, status: {}'.format(request.status_code))
+            return False
+
         content = json.loads(request.content)
 
-        # validate json format fixer api
-        validate(content, FIXER_CURRENCY_JSON_SCHEMA)
+    # validate json format fixer api
+    validate(content, FIXER_CURRENCY_JSON_SCHEMA)
 
-        for currency, value in content['rates'].items():
-            currency_rate = CurrencyRate(
-                currency_name = currency,
-                currency_base_name = content['base'],
-                date = content['date'],
-                rate = value,
-            )
-            currency_rate.save()
+    # store rate in db
+    for currency, value in content['rates'].items():
+        currency_rate = CurrencyRate(
+            currency_name = currency,
+            currency_base_name = content['base'],
+            date = content['date'],
+            rate = value,
+        )
+        currency_rate.save()
 
     return True
