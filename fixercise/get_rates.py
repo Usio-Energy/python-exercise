2277356adc3892630b75fee4a819aa5787cc77be
Rikk Hill <rikk@unashamedly.co.uk>
2018-09-06 12:48:46 +0100

    Added historical rates API call

--- fixercise/get_rates.py
+++ fixercise/get_rates.py
@@ -4,12 +4,28 @@ import os
 # For dev purposes, let's get this from an envvar
 FIXER_API_KEY = os.getenv("FIXER_API_KEY", '')
 
+# TODO: refactor this
+
 
 def get_current_rates():
     # TODO: extract this URL somewhere
     response = requests.get(
         "http://data.fixer.io/api/latest?access_key=%s&format=1"
         % FIXER_API_KEY)
+
+    # TODO: handle different status codes
+
+    return response
+
+
+def get_historic_rates(historic_date):
+    """Gets rates from a historic date.
+        historic_date is ISO date-formatted string YYYY-MM-DD
+    """
+    # TODO: extract this URL somewhere
+    response = requests.get(
+        "http://data.fixer.io/api/%slatest?access_key=%s&format=1"
+        % (historic_date, FIXER_API_KEY))
 
     # TODO: handle different status codes
 
--- tests/test_get_rate.py
+++ tests/test_get_rate.py
@@ -1,6 +1,7 @@
 import unittest
+from datetime import datetime
 from unittest.mock import patch
-from fixercise.get_rates import get_current_rates
+from fixercise.get_rates import *
 
 import tests.testutils as testutils
 
@@ -14,13 +15,32 @@ class TestGetRates(unittest.TestCase):
         response = get_current_rates()
         self.assertEqual(200,
                          response.status_code,
-                         "Happy status code comes back 200")
+                         "Happy status code should come back 200")
 
-        self.assertTrue(response.json(), "Happy response has a body")
+        self.assertTrue(response.json(), "Happy response should have a body")
+        self.assertTrue(response.json()["rates"], "Happy response should have rates")
 
+    @patch('fixercise.get_rates.requests.get')
+    def test_get_historical_rate_happy(self, mock_response):
+        """Check "happy" historic rate call."""
+        historic_date = datetime(2018, 1, 1)
+        historic_iso_date = historic_date.strftime('%Y-%m-%d')
+        mock_response.return_value = testutils.mock_api_response(200, when=historic_date)
+        response = get_historic_rates(historic_iso_date)
+        self.assertEqual(200,
+                         response.status_code,
+                         "Happy status code should come back 200")
+
+        self.assertTrue(response.json(), "Happy response should have a body")
+        self.assertTrue(response.json()["rates"], "Happy response should have rates")
+        self.assertEqual(historic_iso_date, response.json()["date"],
+                         "Happy response date should match request date")
+
+    @patch('fixercise.get_rates.requests.get')
     def test_get_rates_bad_api_call(self, mock_response):
         pass
 
+    @patch('fixercise.get_rates.requests.get')
     def test_get_rates_bad_status(self, mock_response):
         pass
 
